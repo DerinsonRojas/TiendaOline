67dc5058673923655544292c20934da8b6819492
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-20 00:33:00 +0200

    Uso de vista de formulario para enviar datos y recibir otra vista dando las gracias

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -12,12 +12,23 @@ def buscar(request):
         #mensaje="Artículo buscado: %r"%request.GET["prd"]
         producto=request.GET["prd"]
 
-        articulos=Articulos.objects.filter(nombre__icontains=producto)
+        if len(producto)>20:
+            mensaje="Texto de busqueda demasiado largo"
+        else:
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
 
-        return render (request, "resultados_busqueda.html",{"articulos":articulos, "query":producto})
+            return render (request, "resultados_busqueda.html",{"articulos":articulos, "query":producto})
     else:
 
         mensaje="No has introducido nada"
 
     return HttpResponse(mensaje)
+
+def contacto(request):
+
+    if request.method=="POST":
+
+        return render(request, "gracias.html")
+
+    return render(request, "contacto.html")
 # Create your views here.
