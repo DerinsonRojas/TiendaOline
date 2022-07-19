f7a0c169c59463c29c2aefbf9814ff56612b85fb
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-19 20:58:12 +0200

    Consultas basicas de BD mediante formularios

--- TiendaOnline/urls.py
+++ TiendaOnline/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
+from gestionPedidos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('busqueda_productos/', views.busqueda_productos),
+    path('buscar/', views.buscar),
 ]
--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from gestionPedidos.models import Articulos
 
+
+def busqueda_productos(request):
+    return render(request, "busqueda_productos.html")
+
+def buscar(request):
+
+    if request.GET["prd"]:
+        #mensaje="Artículo buscado: %r"%request.GET["prd"]
+        producto=request.GET["prd"]
+
+        articulos=Articulos.objects.filter(nombre__icontains=producto)
+
+        return render (request, "resultados_busqueda.html",{"articulos":articulos, "query":producto})
+    else:
+
+        mensaje="No has introducido nada"
+
+    return HttpResponse(mensaje)
 # Create your views here.
